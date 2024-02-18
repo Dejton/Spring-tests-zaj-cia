51d8961bb72031e746930bf36b22b9ccde1301a9
jarekczaplicki <jarekczaplicki@tlen.pl>
2024-02-18 14:53:04 +0100

    Initial commit

---
+++ src/main/java/com/spring_boot_testing_142/SpringBootTesting142Application.java
@@ -0,0 +1,13 @@
+package com.spring_boot_testing_142;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTesting142Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTesting142Application.class, args);
+    }
+
+}
